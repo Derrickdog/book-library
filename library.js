5c9f7a577e9184e7c9b1bd02c39aa577ecb8de28
Derrick <derrickryan10@gmail.com>
2022-06-10 10:52:48 -0400

    Finish book library

--- library.js
+++ library.js
@@ -1,3 +1,78 @@
+// Display books with DOM
+function Book(title, author, numPages, haveRead){
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.haveRead = haveRead;
+}
+
+Book.prototype.getInfo = function() {
+    return this.title + " by " + this.author + ", " + this.numPages + " pages ";
+}
+
+document.getElementById("bookForm").addEventListener("submit", addBookToLibrary);
+document.getElementById("bookList").addEventListener("click", removeBook);
+document.getElementById("bookList").addEventListener("click", toggleRead);
+
+function addBookToLibrary(e){
+    e.preventDefault();
+
+    //new book
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const numPages = document.getElementById("numPages").value;
+    const haveRead = document.getElementById("haveRead").checked;
+    const newBook = new Book(title, author, numPages, haveRead);
+    
+    //create li
+    const li = document.createElement("li");
+    li.className = "list-group-item";
+    li.textContent = newBook.getInfo();
+
+    //create read badge
+    const badge = document.createElement("span");
+    if(haveRead){
+        badge.className = "badge bg-success readBtn";
+        badge.textContent = "Read";
+    }
+    else{
+        badge.className = "badge bg-secondary readBtn";
+        badge.textContent = "Not Read";
+    }
+
+    //create delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "btn btn-danger btn-sm float-end deleteBtn";
+    deleteButton.textContent = "X";
+
+    //add items
+    li.appendChild(badge);
+    li.appendChild(deleteButton);
+    document.getElementById("bookList").appendChild(li);
+}
+
+function removeBook(e){
+    if(e.target.classList.contains("deleteBtn")){
+        const li = e.target.parentElement;
+        document.getElementById("bookList").removeChild(li);
+    }
+}
+
+function toggleRead(e){
+    if(e.target.classList.contains("readBtn")){
+        const badge = e.target;
+        if(badge.textContent === "Not Read"){
+            badge.className = "badge bg-success readBtn";
+            badge.textContent = "Read";
+        }
+        else{
+            badge.className = "badge bg-secondary readBtn";
+            badge.textContent = "Not Read";
+        }
+    }
+}
+
+/*
 // Log books with prompt
 let myLibrary = [];
 
@@ -18,10 +93,6 @@ function addBookToLibraryPrompt(){
     myLibrary.push(new Book(title, author, numPages, haveRead));
 }
 
-Book.prototype.getInfo = function() {
-    return this.title + " by " + this.author + ", " + this.numPages + " pages";
-}
-
 function logBooks(){
     for(let book of myLibrary){
         console.log(book.getInfo());
@@ -30,47 +101,4 @@ function logBooks(){
 
 // const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "No");
 // console.log(book1.getInfo());
-
-// Display books with DOM
-function Book(title, author, numPages, haveRead){
-    this.title = title;
-    this.author = author;
-    this.numPages = numPages;
-    this.haveRead = haveRead;
-}
-
-document.getElementById("bookForm").addEventListener("submit", addBookToLibrary);
-document.getElementById("bookList").addEventListener("click", removeBook);
-
-function addBookToLibrary(e){
-    e.preventDefault();
-
-    //new book
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-    const numPages = document.getElementById("numPages").value;
-    const haveRead = document.getElementById("haveRead").checked;
-    const newBook = new Book(title, author, numPages, haveRead);
-    myLibrary.push(newBook);
-    
-    //create li
-    const li = document.createElement("li");
-    li.className = "list-group-item";
-    li.textContent = newBook.getInfo();
-
-    //create delete button
-    const deleteButton = document.createElement("button");
-    deleteButton.className = "btn btn-danger btn-sm float-end deleteBtn";
-    deleteButton.textContent = "X";
-
-    //add items
-    li.appendChild(deleteButton);
-    document.getElementById("bookList").appendChild(li);
-}
-
-function removeBook(e){
-    if(e.target.classList.contains("deleteBtn")){
-        const li = e.target.parentElement;
-        document.getElementById("bookList").removeChild(li);
-    }
-}
+*/
