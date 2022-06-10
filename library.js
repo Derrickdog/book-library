c21bfc5517cfd63f98d59fd6cef7eba03ddd99a6
Derrick <derrickryan10@gmail.com>
2022-06-10 09:46:53 -0400

    Add javascript

--- library.js
+++ library.js
@@ -1,3 +1,6 @@
+// const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "No");
+// console.log(book1.getInfo());
+
 let myLibrary = [];
 
 function Book(title, author, numPages, haveRead){
@@ -7,21 +10,42 @@ function Book(title, author, numPages, haveRead){
     this.haveRead = haveRead;
 }
 
-Book.prototype.getInfo = function() {
+Book.prototype.getInfoRead = function() {
     let haveReadString;
     (this.haveRead.toLowerCase() === "yes") ? haveReadString = "read" : haveReadString = "not read yet";
     return this.title + " by " + this.author + ", " + this.numPages + " pages, " + haveReadString;
 }
 
-// const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "No");
-// console.log(book1.getInfo());
+Book.prototype.getInfo = function() {
+    return this.title + " by " + this.author + ", " + this.numPages + " pages";
+}
 
-function addBookToLibrary(){
-    let title = window.prompt("Enter Title");
-    let author = window.prompt("Enter Author");
-    let numPages = window.prompt("Enter Number of Pages");
-    let haveRead = window.prompt("Have you read this book yet? (yes or no)");
+// DOM
+document.getElementById("bookForm").addEventListener("submit", addBookToLibrary);
+// document.getElementById("addButton").addEventListener("click", addBookToLibraryPrompt);
+// document.getElementById("logButton").addEventListener("click", logBooks);
+
+function addBookToLibraryPrompt(){
+    const title = window.prompt("Enter Title");
+    const author = window.prompt("Enter Author");
+    const numPages = window.prompt("Enter Number of Pages");
+    const haveRead = window.prompt("Have you read this book yet? (yes or no)");
     myLibrary.push(new Book(title, author, numPages, haveRead));
+}
+
+function addBookToLibrary(e){
+    e.preventDefault();
+    
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const numPages = document.getElementById("numPages").value;
+    const newBook = new Book(title, author, numPages, "no");
+    myLibrary.push(newBook);
+    
+    const li = document.createElement("li");
+    li.className = "list-group-item";
+    li.textContent = newBook.getInfo();
+    document.getElementById("bookList").appendChild(li);
 }
 
 function logBooks(){
@@ -30,6 +54,4 @@ function logBooks(){
     }
 }
 
-// DOM
-document.getElementById("addButton").addEventListener("click", addBookToLibrary);
-// document.getElementById("logButton").addEventListener("click", logBooks);
+
